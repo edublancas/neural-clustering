757237a6f88fe3fe76d4dced16163805e2956756
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2017-10-08 23:47:18 -0400

    adds basic pkg structure

---
+++ setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import io
+import re
+import ast
+from glob import glob
+import os
+import sys
+from os.path import basename
+from os.path import splitext
+from shutil import rmtree
+
+from setuptools import find_packages, Command, setup
+
+NAME = 'Neural noise'
+DESCRIPTION = 'Modeling noise from multielectrode array readings'
+URL = 'https://github.com/edublancas/neural-noise'
+EMAIL = 'e.blancas@columbia.edu'
+AUTHOR = 'Eduardo Blancas'
+LICENSE = 'GPL3'
+
+REQUIRED = [
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('src/neural_noise/__init__.py', 'rb') as f:
+    VERSION = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds...')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution...')
+        os.system('{0} setup.py sdist bdist_wheel '
+                  '--universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine...')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    url=URL,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    install_requires=REQUIRED,
+    entry_points={
+    },
+)
