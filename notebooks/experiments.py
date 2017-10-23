dbdc92bb60f453dae6918ced0da021fb0253a674
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2017-10-23 17:07:55 -0400

    basic yass pipeline

---
+++ notebooks/experiments.py
@@ -0,0 +1,22 @@
+import logging
+
+import matplotlib.pyplot as plt
+
+import yass
+from yass import preprocess
+from yass import process
+
+# configure logging module to get useful information
+logging.basicConfig(level=logging.DEBUG)
+
+# set yass configuration parameters
+yass.set_config('config.yaml')
+
+# run preprocessor
+score, clr_idx, spt = preprocess.run()
+
+# run processor
+spike_train, spt_left, templates = process.run(score, clr_idx, spt)
+
+plt.plot(templates[0, :, :])
+plt.show()
---
+++ src/neural_noise/process.py
@@ -0,0 +1,2 @@
+"""Functions for processing noise from data
+"""
