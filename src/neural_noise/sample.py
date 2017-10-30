2eeaec62c2238f80b5260d2bf7f8dc8e2ac83d84
Eduardo Blancas <fkq8@blancas.io>
2017-10-30 03:39:04 +0000

    fixes sample function

--- src/neural_noise/sample.py
+++ src/neural_noise/sample.py
@@ -5,13 +5,10 @@ def sample(arr, percentage, dim=0):
     N = arr.shape[dim]
     total_dims = len(arr.shape)
     idx = np.random.choice(N, int(percentage*N), replace=False)
-    
-    def _idx(i, idx):
-        return idx if i == dim else None    
 
-    slices = [_idx(i, idx) for i, idx in enumerate(total_dims)]
+    def _idx(i):
+        return idx if i == dim else slice(0, arr.shape[i])
+
+    slices = [_idx(i) for i in range(total_dims)]
     return arr[slices]
-
-
-
 
