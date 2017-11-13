b6213840428aee0a39ad961d6d463fd18fe3cbc6
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-11-13 17:23:04 -0500

    cleans up setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-
 import io
 import re
 import ast
 from glob import glob
 import os
-import sys
 from os.path import basename
 from os.path import splitext
-from shutil import rmtree
 
-from setuptools import find_packages, Command, setup
+from setuptools import find_packages, setup
 
 NAME = 'neural-clustering'
-DESCRIPTION = 'Modeling noise from multielectrode array readings'
+DESCRIPTION = 'Clustering from multielectrode array readings using Edward'
 URL = 'https://github.com/edublancas/neural-noise'
 EMAIL = 'e.blancas@columbia.edu'
 AUTHOR = 'Eduardo Blancas'
@@ -33,41 +29,6 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('src/neural_noise/__init__.py', 'rb') as f:
     VERSION = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel '
-                  '--universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPi via Twine...')
-        os.system('twine upload dist/*')
-
-        sys.exit()
-
 
 setup(
     name=NAME,
