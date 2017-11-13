5e714def3b0e3e208d310b4f4bb0bf2ff9f5cb94
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-11-13 14:41:25 -0500

    script for counting notebook length

---
+++ src/neural_noise/count.sh
@@ -0,0 +1,10 @@
+# https://gist.github.com/minrk/6176788
+
+# convert notebooks to md
+jupyter-nbconvert --to markdown notebooks/1-intro.ipynb
+jupyter-nbconvert --to markdown notebooks/2-data-loading.ipynb
+jupyter-nbconvert --to markdown notebooks/3-dpmm-v1.ipynb
+jupyter-nbconvert --to markdown notebooks/4-criticism.ipynb
+
+# count words
+wc -w notebooks/1-intro.md notebooks/2-data-loading.md notebooks/3-dpmm-v1.md notebooks/4-criticism.md
