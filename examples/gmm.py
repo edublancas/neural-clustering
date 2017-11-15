e3dd90f002d76755c59883b89dbefb24eb56c941
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-11-15 12:02:11 -0500

    complete gmm example

--- examples/gmm.py
+++ examples/gmm.py
@@ -1,13 +1,32 @@
 """
-Gaussian mixture model with Edward
+Gaussian mixture modelusing Edward
+
+Based on:
+http://edwardlib.org/tutorials/unsupervised
 """
+import numpy as np
 import tensorflow as tf
 import edward as ed
 from edward.models import (Dirichlet, InverseGamma, MultivariateNormalDiag,
                            Normal, ParamMixture, Empirical)
+import matplotlib.pyplot as plt
 
 
-# x_train = build_toy_dataset(N)
+ed.set_seed(0)
+
+
+def build_toy_dataset(N):
+    pi = np.array([0.4, 0.6])
+    mus = [[5, 5], [-5, -5]]
+    stds = [[1.0, 1.0], [1.0, 1.0]]
+    x = np.zeros((N, 2), dtype=np.float32)
+
+    for n in range(N):
+        k = np.argmax(np.random.multinomial(1, pi))
+        x[n, :] = np.random.multivariate_normal(mus[k], np.diag(stds[k]))
+
+    return x
+
 
 ##############
 # Parameters #
@@ -16,30 +35,28 @@ from edward.models import (Dirichlet, InverseGamma, MultivariateNormalDiag,
 # number of data points
 N = 500
 # number of components
-K = 8
+K = 2
 # dimensionality of data
 D = 2
 # number of MCMC samples
-T = 1000
+T = 5000
 
 
-##########
-# Priors #
-##########
+x_train = build_toy_dataset(N)
 
-# prior distribution of the pi_k
-pi = Dirichlet(tf.ones(K))
-
-# prior over the normal means
-mu = Normal(tf.zeros(D), tf.ones(D), sample_shape=K)
-
-# prior over the variances
-sigmasq = InverseGamma(tf.ones(D), tf.ones(D), sample_shape=K)
+# plot toy dataset
+plt.scatter(x_train[:, 0], x_train[:, 1])
+plt.show()
 
 
 #########
 # Model #
 #########
+
+pi = Dirichlet(tf.ones(K))
+mu = Normal(tf.zeros(D), tf.ones(D), sample_shape=K)
+sigmasq = InverseGamma(tf.ones(D), tf.ones(D), sample_shape=K)
+
 
 x = ParamMixture(pi, {'loc': mu, 'scale_diag': tf.sqrt(sigmasq)},
                  MultivariateNormalDiag,
@@ -57,18 +74,18 @@ qmu = Empirical(tf.Variable(tf.zeros([T, K, D])))
 qsigmasq = Empirical(tf.Variable(tf.ones([T, K, D])))
 qz = Empirical(tf.Variable(tf.zeros([T, N], dtype=tf.int32)))
 
-
+# http://edwardlib.org/api/ed/Gibbs
 inference = ed.Gibbs({pi: qpi, mu: qmu, sigmasq: qsigmasq, z: qz},
                      data={x: x_train})
-
 inference.initialize()
 
 sess = ed.get_session()
-tf.global_variables_initializer().run()
+init = tf.global_variables_initializer()
+init.run()
+
 
 t_ph = tf.placeholder(tf.int32, [])
 running_cluster_means = tf.reduce_mean(qmu.params[:t_ph], 0)
-
 
 for _ in range(inference.n_iter):
     info_dict = inference.update()
@@ -80,19 +97,40 @@ for _ in range(inference.n_iter):
         print(sess.run(running_cluster_means, {t_ph: t - 1}))
 
 
-# Calculate likelihood for each data point and cluster assignment,
-# averaged over many posterior samples. ``x_post`` has shape (N, 100, K, D).
-mu_sample = qmu.sample(100)
-sigmasq_sample = qsigmasq.sample(100)
+#############
+# Criticism #
+#############
+
+SC = 200
+
+mu_sample = qmu.sample(SC)
+sigmasq_sample = qsigmasq.sample(SC)
 x_post = Normal(loc=tf.ones([N, 1, 1, 1]) * mu_sample,
                 scale=tf.ones([N, 1, 1, 1]) * tf.sqrt(sigmasq_sample))
-x_broadcasted = tf.tile(tf.reshape(x_train, [N, 1, 1, D]), [1, 100, K, 1])
+x_broadcasted = tf.tile(tf.reshape(x_train, [N, 1, 1, D]), [1, SC, K, 1])
 
-# Sum over latent dimension, then average over posterior samples.
-# ``log_liks`` ends up with shape (N, K).
 log_liks = x_post.log_prob(x_broadcasted)
 log_liks = tf.reduce_sum(log_liks, 3)
 log_liks = tf.reduce_mean(log_liks, 1)
 
-
 clusters = tf.argmax(log_liks, 1).eval()
+
+plt.scatter(x_train[:, 0], x_train[:, 1], c=clusters)
+plt.title("Predicted cluster assignments")
+plt.show()
+
+
+x_pred = ed.copy(x, {pi: qpi, mu: qmu, sigmasq: qsigmasq, z: qz})
+
+# log-likelihood performance
+ed.evaluate('log_likelihood', data={x_pred: x_train})
+
+# ppc
+y_rep, y = ed.ppc(lambda xs, mus: tf.reduce_mean(xs[x_pred]),
+                  data={x_pred: x_train},
+                  n_samples=1000)
+
+ed.ppc_stat_hist_plot(y[0], y_rep,
+                      stat_name=r'$T \equiv$mean', bins=10)
+
+plt.show()
