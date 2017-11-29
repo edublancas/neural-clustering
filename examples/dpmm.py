46ee7b0b3b482887ad3428cfc2c5c8cd1b77265d
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-11-28 21:39:07 -0500

    final dpmm version - only works with klqp but inference is bad, gibbs is not fully supported. at least i learned a lot about edward and mixure models...

--- examples/dpmm.py
+++ examples/dpmm.py
@@ -2,34 +2,42 @@
 Truncated DPMM using Edward.
 
 It's been several attempts to implement truncated DPMM on Edward but there are
-many problems. First, inference cannot properly be made with MCMC since Edward
-throws this error when trying to make inference:
+many problems. First, inference in the joint version (using ParamMixture)
+cannot properly be made with MCMC since Edward throws this error when
+trying to make inference:
 
 ValueError: 'transform' does not handle supports of type 'categorical'
 
 Gibbs throws a different error:
 
-NotImplementedError: Conditional distribution has sufficient statistics (...)
+NotImplementedError: Number of components must be statically determined.
 
-Seems like the only options is to use variational inference. Here I am using
-KLqp, I bumped into several errors which have already been reported in
-discourse, I managed to have a working truncated DPMM model but inference
-is still bad, not sure exactly why. Since I had limited time and invested
-too much time on this I decided to also use GMM.
+Seems like the only option (for the joint version) is to use variational
+inference. I tried using KLqp, but bumped into several errors which have
+already been reported in discourse, I managed to have a working truncated DPMM
+with KLqp but inference is bad, more on that here:
+https://discourse.edwardlib.org/t/gaussian-mixture-with-covariance-matrix-via-klqp-method/277/2
 
-Based on:
+Seems like the way to go is to use Gibbs, but as I mention before, Gibbs
+foes not work in the joint model. I also tried the collapsed version
+(using Mixture) but inference is bad, and with Gibbs I get this error:
+
+NotImplementedError: conjugate_log_prob not implemented for
+<class 'abc.Mixture'>
+
+References:
 https://gist.github.com/dustinvtran/d8cc112636b219776621444324919928
 http://edwardlib.org/tutorials/unsupervised
 http://docs.pymc.io/notebooks/dp_mix.html
 https://discourse.edwardlib.org/t/dpm-model-for-clustering/97/6
 https://discourse.edwardlib.org/t/variational-inference-for-dirichlet-process-mixtures/251
 
-Cannot use MetropolisHastings
+Mixtures and MetropolisHastings:
 https://github.com/blei-lab/edward/blob/master/examples/mixture_gaussian_mh.py
 """
 from edward.models import (Normal, MultivariateNormalDiag, Beta,
-                           InverseGamma,  ParamMixture, Empirical,
-                           Gamma, Categorical, Mixture)
+                           ParamMixture, Empirical, Gamma, Categorical,
+                           Mixture)
 import edward as ed
 import numpy as np
 import tensorflow as tf
@@ -54,7 +62,6 @@ def stick_breaking(v):
     remaining_pieces = tf.concat([tf.ones(1), tf.cumprod(1.0 - v)[:-1]], 0)
     weights = v * remaining_pieces
     return tf.concat([weights, [1.0 - tf.reduce_sum(weights)]], axis=0)
-
 
 # def stick_breaking(v):
 #     remaining_pieces = tf.concat([tf.ones(1), tf.cumprod(1.0 - v)[:-1]], 0)
@@ -81,10 +88,12 @@ plt.show()
 # Model
 beta = Beta(tf.ones(T), tf.ones(T))
 
-# alpha = Gamma(tf.ones(T), tf.ones(T))
-# beta = Beta(tf.ones(T), alpha)
+alpha = Gamma(tf.ones(T), tf.ones(T))
+beta = Beta(tf.ones(T), alpha)
 
 pi = stick_breaking(beta)
+
+pi = tf.constant([0.39, 0.01, 0.60])
 
 mu = Normal(tf.zeros(D), tf.ones(D), sample_shape=K)
 
@@ -94,41 +103,44 @@ mu = Normal(tf.zeros(D), tf.ones(D), sample_shape=K)
 # sigmasq = InverseGamma(tf.ones(D), tf.ones(D), sample_shape=K)
 sigmasq = tf.ones((K, D))
 
-# pi = tf.constant([0.39, 0.01, 0.60])
+# joint model
 x = ParamMixture(pi, {'loc': mu, 'scale_diag': tf.sqrt(sigmasq)},
                  MultivariateNormalDiag,
                  sample_shape=N)
 z = x.cat
+
+# collapsed model
+cat = Categorical(probs=pi, sample_shape=N)
+components = [MultivariateNormalDiag(tf.zeros(D), tf.ones(D), sample_shape=N)
+              for k in range(K)]
+x = Mixture(cat, components, sample_shape=N)
+
 
 # plot prior
 x_original = x.sample(500).eval()
 sns.jointplot(x_original[:, 0], x_original[:, 1], kind='kde')
 plt.show()
 
-
-# Alternative model
-cat = Categorical(probs=pi, sample_shape=N)
-components = [MultivariateNormalDiag(tf.zeros(D), tf.ones(D), sample_shape=N)
-              for k in range(K)]
-x = Mixture(cat, components, sample_shape=N)
-
 # Inference with KLqp
-# qsigmasq = InverseGamma(tf.Variable(tf.zeros([K, D])), tf.Variable(tf.zeros([K, D])))
-# qalpha = Gamma(tf.Variable(tf.ones(T)), tf.Variable(tf.ones(T)))
-
+# qsigmasq = InverseGamma(tf.Variable(tf.zeros([K, D])),
+#                         tf.Variable(tf.zeros([K, D])))
+qalpha = Gamma(tf.Variable(tf.ones(T)), tf.Variable(tf.ones(T)))
 qbeta = Beta(tf.ones([T]), tf.nn.softplus(tf.Variable(tf.ones([T]))))
 
 # m = np.array([[-15.0, -10.0], [5.0, 0.0], [20.0, 20.0]]).astype('float32')
 qmu = Normal(tf.Variable(tf.zeros([K, D])), tf.ones([K, D]))
-
 qz = Categorical(tf.nn.softmax(tf.Variable(tf.zeros([N, K]))))
 
+inference = ed.KLqp({mu: qmu}, data={x: x_train})
 
 inference = ed.KLqp({mu: qmu, beta: qbeta}, data={x: x_train})
-# inference = ed.KLqp({mu: qmu, z: qz, beta: qbeta}, data={x: x_train})
-# inference = ed.KLqp({mu: qmu, z: qz, alpha: qalpha}, data={x: x_train})
+inference = ed.KLqp({mu: qmu, alpha: qalpha}, data={x: x_train})
+
+inference = ed.KLqp({mu: qmu, z: qz, beta: qbeta}, data={x: x_train})
+inference = ed.KLqp({mu: qmu, z: qz, alpha: qalpha}, data={x: x_train})
 
 inference.initialize(n_samples=3, n_iter=5000, n_print=100)
+
 
 init = tf.global_variables_initializer()
 init.run()
@@ -144,3 +156,42 @@ for _ in range(inference.n_iter):
         print(sess.run(qmu.mean()))
         print("Beta")
         print(qbeta.concentration0.eval())
+
+
+S = 5000
+qmu = Empirical(tf.Variable(tf.zeros([S, K, D])))
+qbeta = Empirical(tf.Variable(tf.zeros([S, T])))
+qalpha = Empirical(tf.Variable(tf.zeros([S, T])))
+qz = Empirical(tf.Variable(tf.zeros([S, N], dtype=tf.int32)))
+
+# below doesn't work, so I need to use the collapsed version...
+# inference = ed.SGLD({mu: qmu, z: qz}, data={x: x_train})
+
+inference = ed.SGLD({mu: qmu, beta: qbeta}, data={x: x_train})
+inference = ed.SGLD({mu: qmu, alpha: qalpha}, data={x: x_train})
+
+# inference for joint model throws NotImplementedError: Number of components
+# must be statically determined.
+inference = ed.Gibbs({mu: qmu, z: qz, beta: qbeta}, data={x: x_train})
+
+# inference for collapsed model throws error
+# NotImplementedError: conjugate_log_prob not implemented for
+# <class 'abc.Mixture'>
+inference = ed.Gibbs({mu: qmu, beta: qbeta}, data={x: x_train})
+
+inference.initialize()
+
+init = tf.global_variables_initializer()
+init.run()
+
+t_ph = tf.placeholder(tf.int32, [])
+running_cluster_means = tf.reduce_mean(qmu.params[:t_ph], 0)
+
+for _ in range(inference.n_iter):
+    info_dict = inference.update()
+    inference.print_progress(info_dict)
+    t = info_dict['t']
+
+    if t % inference.n_print == 0:
+        print("\nInferred cluster means:")
+        print(sess.run(running_cluster_means, {t_ph: t - 1}))
